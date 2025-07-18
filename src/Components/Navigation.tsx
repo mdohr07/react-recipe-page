79e84e67503b0df7c6613a89ac8f3459418088f1
mdohr07 <mdohr07@gmail.com>
2025-07-18 12:22:50 +0200

    Add menu onClick

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -1,21 +1,69 @@
-import React, { type FC } from 'react'
+import React, { useState, type FC } from "react";
 
 const Navigation: FC = () => {
-  return (
-    <div className="navbar bg-primary text-primary-content p-4">
-  <div className="flex-1 pl-4 align-left">
-    <a className=" text-xl text-base-100 logo" href="/">
-        <h1>nom²</h1>
-    </a>
-  </div>
-  <div className="flex-none">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Link</a></li>
-      <li><a>Link</a></li>
-    </ul>
-  </div>
-</div>
-  )
-}
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuOptions = [
+    {
+      text: "Link 1",
+      href: "#",
+    },
+    {
+      text: "Link 2",
+      href: "#",
+    },
+    {
+      text: "Link 3",
+      href: "#",
+    },
+  ];
 
-export default Navigation
+  return (
+    <header>
+      <div className="navbar bg-primary text-primary-content p-4">
+        <div className="flex-1 pl-4 align-left">
+          <a className=" text-xl text-base-100 logo" href="/">
+            <h1>nom²</h1>
+          </a>
+        </div>
+
+        <div className="flex-none">
+          <button
+            className="btn btn-primary btn-lg"
+            onClick={() => setOpenMenu(!openMenu)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block h-5 w-5 stroke-current"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>{" "}
+            </svg>
+          </button>
+        </div>
+
+        {openMenu && (
+          <ul className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+            <li>
+              <a href="#">Link 1</a>
+            </li>
+            <li>
+              <a href="#">Link 2</a>
+            </li>
+            <li>
+              <a href="#">Link 3</a>
+            </li>
+          </ul>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default Navigation;
